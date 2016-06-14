37127ce8208c44e3759bff1618193eb9683788fc
Daniel Schmitz <danieljfa@gmail.com>
2016-06-14 18:56:17 -0300

    .

--- blog/src/Home.vue
+++ blog/src/Home.vue
@@ -1,21 +1,22 @@
 <template>
   <div v-show="showProgress" class="progress">
-      <div class="indeterminate"></div>
+    <div class="indeterminate"></div>
   </div>
   <div class="row" v-for="post in posts">
-        <div class="col s12">
-          <div class="card  blue lighten-5">
-            <div class="card-content black-text">
-              <span class="card-title">{{post.title}}</span>
-              <p>{{post.text}}</p>
-            </div>
-            <div class="card-action">
-              <span><i class="material-icons">perm_identity</i> {{post.user.name}}</span>
-              <!-- <a href="#">This is a link</a> -->
-            </div>
-          </div>
+    <div class="col s12">
+      <div class="card  blue lighten-5">
+        <div class="card-content black-text">
+          <span class="card-title">{{post.title}}</span>
+          <p>{{post.text}}</p>
+        </div>
+        <div class="card-action">
+          <span><i class="material-icons">perm_identity</i> {{post.user.name}}</span>
+          <a href="#" @click="remove(post)" class="right blue-text" v-if="login.token!=null && login.id==post.user._id">Remove</a>
         </div>
       </div>
+    </div>
+  </div>
+
 </template>
 <script>
   import Auth from './auth.js'
@@ -23,13 +24,26 @@
     data (){
       return {
         posts: null,
-        showProgress:true
+        showProgress:true,
+        login:Auth.getLogin()
       }
     },
     created: function(){
       //console.log(Auth.getLogin());
       this.showProgress=true;
-      this.$http.get('/api/posts').then(function(response){
+     this.loadPosts();
+    },
+    methods: {
+      remove: function(post){
+        post.token = Auth.getLogin().token;
+        this.$http.delete('/api/posts/'+post._id,post).then(function(response){
+          this.loadPosts();
+      },function(error){
+        //console.log(error)
+        Materialize.toast('Error: ' + error.data.message, 3000)
+      });
+      },loadPosts:function(){
+       this.$http.get('/api/posts').then(function(response){
           this.showProgress=false
           this.posts = response.data
           console.log(this.posts);
@@ -38,5 +52,6 @@
           Materialize.toast('Error: ' + error.statusText, 3000)
       })
     }
+  }
   }
 </script>
