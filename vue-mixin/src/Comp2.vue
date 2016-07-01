2d3f76b0c39b90927054e17367b03fcc719ea121
Daniel Schmitz <danieljfa@gmail.com>
2016-07-01 08:14:43 -0300

    Comp2.vue mixim merge

--- vue-mixin/src/Comp2.vue
+++ vue-mixin/src/Comp2.vue
@@ -3,6 +3,9 @@
 import {eventMixin} from './eventMixin'
 
 export default{
-    mixins: [eventMixin]
+    mixins: [eventMixin],
+    created : function() {
+        console.log(" event Comp 2 outside mixin ")
+    }
 }
 </script>
