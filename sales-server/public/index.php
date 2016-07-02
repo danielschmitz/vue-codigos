88aa335e8f4b31c0dde6c78a57a9cbe51f1ed0d2
Daniel Schmitz <danieljfa@gmail.com>
2016-07-02 08:24:58 -0300

    phpslim cors

--- sales-server/public/index.php
+++ sales-server/public/index.php
@@ -4,8 +4,16 @@ require '../database.php';
 
 $app = new \Slim\App;
 
+$corsOptions = array(
+    "origin" => "*",
+    "exposeHeaders" => array("Content-Type", "X-Requested-With", "X-authentication", "X-client"),
+    "allowMethods" => array('GET', 'POST', 'PUT', 'DELETE', 'OPTIONS')
+);
+$cors = new \CorsSlim\CorsSlim($corsOptions);
+$app->add($cors);
+
 require 'tests.php';
-//require 'user.php'
+require 'user.php';
 //require 'employee.php'
 //require 'customer.php'
 //require 'sales.php'
