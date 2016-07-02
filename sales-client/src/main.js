241f0c372071308b7d794d7167b661d7630799a1
Daniel Schmitz <danieljfa@gmail.com>
2016-07-02 08:24:32 -0300

    vue-resource

--- sales-client/src/main.js
+++ sales-client/src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import VueValidator from 'vue-validator'
+import VueResource from 'vue-resource'
 
 Vue.use(VueValidator)
+Vue.use(VueResource)
 
 new Vue({
   el: 'body',
