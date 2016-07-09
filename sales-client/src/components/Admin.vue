f54ca74620a08f211d519aa863c41e72295b626a
Daniel Schmitz <danieljfa@gmail.com>
2016-07-09 10:40:26 -0300

    removendo comentários

--- sales-client/src/components/Admin.vue
+++ sales-client/src/components/Admin.vue
@@ -12,10 +12,7 @@
       </button>
                 <a class="navbar-brand" href="#">Sistema de vendas</a>
             </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
                 <ul class="nav navbar-nav navbar-right">
                     <li v-link-active><a v-link="{ path: '/users' }">Usuários</a></li>
                     <li v-link-active><a v-link="{ path: '/suppliers' }">Fornecedores</a></li>
@@ -24,26 +21,12 @@
                     <li v-link-active><a v-link="{ path: '/sales' }">Vendas</a></li>
                     <li v-link-active><a v-link="{ path: '/pdv' }">PDV</a></li>
                     <li v-link-active><a v-link="{ path: '/logout' }">Logout</a></li>
-                    <!--
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown1 <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
-        -->
                 </ul>
             </div>
-            <!-- /.navbar-collapse -->
         </div>
-        <!-- /.container-fluid -->
     </nav>
     <div class="container">
-         <router-view></router-view>
+        <router-view></router-view>
     </div>
 
 </template>
