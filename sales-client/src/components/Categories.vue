0b1a3ada7d610cc8457f9191aa12f345c41f6f61
daniel-schmitz <danieljfa@gmail.com>
2016-07-14 15:49:28 -0300

    Validação

--- sales-client/src/components/Categories.vue
+++ sales-client/src/components/Categories.vue
@@ -44,37 +44,44 @@
                 </div>
                 <div class="col-md-6">
                     <Error></Error>
-                    <form>
-                        <div class="form-group">
-                            <label for="name">Id</label>
-                            <input type="input" class="form-control" id="id" placeholder="id" v-model="getCategory.id" readonly>
-                        </div>
-                        <div class="form-group">
-                            <label for="name">Nome</label>
-                            <input type="input" class="form-control" id="name" placeholder="Nome" v-model="getCategory.name">
-                        </div>
-                        <button @click.prevent="trySaveCategory" class="btn btn-default" :disabled="isLoading">Salvar</button>
-                        <Loading></Loading>
-                          <button @click.prevent="tryDeleteCategory" class="btn btn-default pull-right" :disabled="isLoading||getCategory.id==null">Apagar</button>
-                    </form>
+                    <validator name="validateForm">
+                        <form>
+                            <div class="form-group">
+                                <label for="name">Id</label>
+                                <input type="input" class="form-control" id="id" placeholder="id" v-model="getCategory.id" readonly>
+                            </div>
+                            <div class="form-group">
+                                <label for="name">Nome</label>
+                                <input type="input" class="form-control" id="name" placeholder="Nome" v-model="getCategory.name" v-validate:name="{ required: true, minlength: 2 }">
+                                <div v-show="$validateForm.invalid">
+                                    <span class="label label-info" v-if="$validateForm.name.required">Campo requerido</span>
+                                    &nbsp;
+                                    <span class="label label-info" v-if="$validateForm.name.minlength">Mínimo 4 caracteres</span>
+                                </div>
+                            </div>
+                            <button @click.prevent="trySaveCategory" class="btn btn-default" :disabled="isLoading||$validateForm.invalid" >Salvar</button>
+                            <Loading></Loading>
+                            <button @click.prevent="tryDeleteCategory" class="btn btn-default pull-right" :disabled="isLoading||getCategory.id==null">Apagar</button>
+                        </form>
+                    </validator>
                 </div>
             </div>
         </div>
     </div>
-    </div>
+</div>
 </template>
 <script>
-import {setCategory,saveCategory,loadCategories,changeCategoriesPage,deleteCategory} from '../vuex/actions.js'
-import {getCategory,getCategories,getTotalCategories,isLoading,getCategoryPage,getItensPerPage} from '../vuex/getters.js'
-import Loading from '../controls/Loading.vue'
-import Error from '../controls/Error.vue'
-import Pagination from '../controls/Pagination.vue'
+    import {setCategory,saveCategory,loadCategories,changeCategoriesPage,deleteCategory} from '../vuex/actions.js'
+    import {getCategory,getCategories,getTotalCategories,isLoading,getCategoryPage,getItensPerPage} from '../vuex/getters.js'
+    import Loading from '../controls/Loading.vue'
+    import Error from '../controls/Error.vue'
+    import Pagination from '../controls/Pagination.vue'
 
-export default{
-    components: {
-			Loading, Error,Pagination
-		},
-    vuex:{
+    export default{
+        components: {
+         Loading, Error,Pagination
+     },
+     vuex:{
         actions:{
             setCategory,saveCategory,loadCategories,changeCategoriesPage, deleteCategory
         },
@@ -98,21 +105,21 @@ export default{
             this.saveCategory(this.keyword);
         },
         tryEdit(category){
-             this.setCategory(category);
-        },
-        onChangePage(page){
+           this.setCategory(category);
+       },
+       onChangePage(page){
           this.changeCategoriesPage(page,this.keyword)
-        },
-        trySearch(){
-            this.changeCategoriesPage(1,this.keyword)
-            this.loadCategories(this.keyword)
-        },
-        tryDeleteCategory(){
-            if (confirm(`Deseja apagar "${this.getCategory.name}"s ?`)){
-                this.deleteCategory(this.keyword)
-            }
+      },
+      trySearch(){
+        this.changeCategoriesPage(1,this.keyword)
+        this.loadCategories(this.keyword)
+    },
+    tryDeleteCategory(){
+        if (confirm(`Deseja apagar "${this.getCategory.name}"s ?`)){
+            this.deleteCategory(this.keyword)
         }
     }
+}
 
 
 }</script>
